2fbeb42f75ea4ea9fbc8d4d45bf3ce9d927b81dd
Gaurav Pratap Singh <gauravpratapsingh321@gmail.com>
2025-04-16 23:05:35 +0530

    commit

---
+++ connect-to-blockchain.js
@@ -0,0 +1,10 @@
+let Web3 = require("web3");
+
+let provider = new Web3.providers.HttpProvider('https://eth-mainnet.g.alchemy.com/v2/upqnG4KpoMpYxAvl94FotRNIaP_0eO9w');
+let web3 = new Web3(provider);
+
+async function getBalance() {
+     const balance = await web3.eth.getBalance('0xf4783E9620fA022b874900D0C833112A30Df5b3f');
+
+     getBalance();
+}
